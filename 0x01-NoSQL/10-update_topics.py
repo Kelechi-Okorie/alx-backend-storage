2c7d054c9ec29c44ba03bd10a20af02d5fd22847
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-03-26 06:03:49 -0700

    Bug fix on task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,6 +2,6 @@
 """ Updates all school topics """
 
 
-def update_topics(mogo_collection, name, topics):
+def update_topics(mongo_collection, name, topics):
     """Update topics of all school collection with a given name"""
-    mongo_collection.update_many({"name": name}, {$set: {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
